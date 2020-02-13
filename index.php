cf58c9ce85f0e6861513b267fa449382ba7d2a8a
Lukylix <lucas.garcia.bobi@gmail.com>
2020-02-13 11:03:32 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+spl_autoload_register(function ($class) {
+  if (file_exists('./core/' . $class . '.php')) :
+    include './core/' . $class . '.php';
+  elseif (file_exists('./models/' . $class . '.php')) :
+    include './models/' . $class . '.php';
+  elseif (file_exists('./controllers/' . $class . '.php')) :
+    include './controllers/' . $class . '.php';
+  endif;
+});
+
+echo "<h1>Plop</h1>";
+phpinfo();

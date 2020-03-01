002646f46d0328a6a3ac349f57d9bee3696edbfa
Lukylix <lucas.garcia.bobi@gmail.com>
2020-03-01 22:17:23 +0100

    Controller done and functional

--- core/Controller.php
+++ core/Controller.php
@@ -1,21 +1,46 @@
 <?php
 
-Class Controller {
-  private $post;
-  private $get;
+class Controller
+{
+  protected $post;
+  protected $get;
 
   function __construct()
   {
     $this->post = $_POST;
     $this->get = $_GET;
   }
-  function render(string $vuePath, $data){
-
+  //Create a vue and inject data inside it
+  final protected function render(string $viewPath, $data = null)
+  {
+    if (is_array($data)){
+      foreach ($data as $key => $val) {
+        ${$key} = $val;
+      }
+    }
+    $extensions = ['php', 'html'];
+    //Remove '/' at the start of the string
+    $viewPath = preg_replace('/^\//', '', $viewPath);
+    foreach ($extensions as $ext) {
+      if (file_exists("views/$viewPath.$ext")) return include("views/$viewPath.$ext");
+    }
+    return false;
   }
-  function getGet(){
+  //Json output
+  final protected function renderJson($data , $prettyJson = true){
+    header('Content-Type: application/json');
+    if ($prettyJson){
+      echo json_encode($data, JSON_FORCE_OBJECT|JSON_PRETTY_PRINT);
+    }else{
+      echo json_encode($data, JSON_FORCE_OBJECT);
+    }
+  }
+  function inputget()
+  {
     return $this->get;
   }
-  function getPost(){
+  function inputPost()
+  {
     return $this->post;
   }
 }
--- core/EntityModel.php
+++ core/EntityModel.php
@@ -1,5 +1,5 @@
 <?php
-abstract class EntityModel
+class EntityModel
 {
   private $dao;
   private $id;
